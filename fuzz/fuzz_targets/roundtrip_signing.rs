56b44a825deeb42b3117351f6be8eda6c4a44091
AljoschaMeyer <mail@aljoscha-meyer.de>
2018-10-29 16:23:59 +0100

    Update roundtrip fuzzers

--- fuzz/fuzz_targets/roundtrip_signing.rs
+++ fuzz/fuzz_targets/roundtrip_signing.rs
@@ -5,14 +5,14 @@ extern crate ssb_legacy_msg;
 extern crate ssb_legacy_msg_data;
 
 use ssb_legacy_msg::{Message, json::{from_legacy, to_legacy_vec}};
-use ssb_legacy_msg_data::value::Value;
+use ssb_legacy_msg_data::value::ContentValue;
 
 fuzz_target!(|data: &[u8]| {
     // This comment keeps rustfmt from breaking the fuzz macro...
-    match from_legacy::<Value>(data) {
+    match from_legacy::<ContentValue>(data) {
         Ok((msg, _)) => {
-            let sign_json = to_legacy_vec(&msg, false);
-            let redecoded = from_legacy::<Value>(&sign_json[..]).unwrap().0;
+            let sign_json = to_legacy_vec(&msg, false).unwrap();
+            let redecoded = from_legacy::<ContentValue>(&sign_json[..]).unwrap().0;
             assert_eq!(msg, redecoded);
         }
 
